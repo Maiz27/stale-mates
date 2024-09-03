e866c993280c3b5985f4bf564ec1fb27e3f92f22
Maged Faiz <magedfaiz98@gmail.com>
2024-09-03 13:29:32 +0200

    feat: add logo imgs
    - add  title & description meta data
    - add logo to header
    - remove svelte favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,14 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>Stale Mates - Play Chess Online</title>
+	<meta
+		name="description"
+		content="Welcome to Stale-Mates, where chess meets fun for all levels."
+	/>
+</svelte:head>
+
 <ModeWatcher />
 
 <div class="min-h-screen">
